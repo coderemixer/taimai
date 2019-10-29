b6517cd38f7a7486bc3f3b8710d9a061542f57ac
Delton Ding <dsh0416@gmail.com>
2019-10-29 22:29:41 +0900

    Fix arch

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,29 +1,31 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <el-button>Element UI enabled</el-button>
-  </div>
+<template lang="pug">
+#app
+  img(src="./assets/logo.png")
+  hello-world(msg="Welcome to Your Vue.js App")
+  el-button
+    | Element UI enabled
 </template>
 
 <script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default {
+@Component({
   name: 'app',
   components: {
     HelloWorld,
   },
-};
+})
+export default class App extends Vue {
+}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
 </style>
--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -1,15 +1,15 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
-import Element from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
 Vue.config.productionTip = false;
 
-Vue.use(Element);
+Vue.use(ElementUI);
 
 new Vue({
   router,
