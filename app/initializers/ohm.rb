fbec3902b6e51a0884df38ea465400f8e5dbe876
Delton Ding <dsh0416@gmail.com>
2020-03-27 12:15:52 +0900

    Update db and redis conn

--- app/initializers/ohm.rb
+++ app/initializers/ohm.rb
@@ -1,2 +1,3 @@
 env = ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'
-Ohm.redis = Redic.new("#{YAML.load_file('app/config/redis.yml')[env]}/database")
+redis_url = YAML.load(ERB.new(File.read('app/config/redis.yml')[env]).result(binding))
+Ohm.redis = Redic.new("#{redis_url}/database")
--- app/initializers/sequel.rb
+++ app/initializers/sequel.rb
@@ -1,5 +1,5 @@
 env = ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'
-DB = Sequel.connect(YAML.load_file('app/config/db.yml')[env])
+DB = Sequel.connect(YAML.load(ERB.new(File.read('app/config/db.yml')[env]).result(binding)))
 DB.extension(:connection_validator)
 DB.pool.connection_validation_timeout = 60
 DB.extension(:pagination)
