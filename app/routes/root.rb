b04192c0e77a3e88141b1d7fd6b5d553ac035560
Delton Ding <dsh0416@gmail.com>
2020-03-27 14:59:53 +0900

    Minor updates

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -1,11 +1,7 @@
 # @!visibility private
 # Leave API docs for API docs
 ROOT_ROUTE = proc do
-  get '' do
-    'Hello World'
-  end
-
-  get '/hello' do
-    'Hello API'
+  get '/status' do
+    'The Service is Running...'
   end
 end
